6fda5e7c5130c7a8293e05ff640847e29ea6824f
Pradeep Kumar <50083639+pradeep9955@users.noreply.github.com>
2021-10-23 22:58:59 +0530

    Add files via upload

---
+++ InverseArr.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+int* inverse(int* arr, int n){
+    // write your code here
+    int* inv = new int[n];
+    for(int i=0;i<n;i++){
+    int val=arr[i];
+    inv[val]=i;    
+    }
+    return inv;
+}
+
+void display(int* arr, int n){
+    for(int i = 0 ; i < n; i++){
+        cout<<arr[i]<<endl;
+    }
+    cout<<endl;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    int* arr = new int[n];
+    for(int i = 0 ; i < n; i++){
+        cin>>arr[i];
+    }
+    
+    int* inv = inverse(arr,n);
+    display(inv,n);
+    
+}
